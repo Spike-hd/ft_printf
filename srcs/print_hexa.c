77cbdb07bbc70364ec7ae927140fec09f4758fcf
Hadrien Duflos <hduflos@c3r2p10.42perpignan.fr>
2024-11-15 14:45:11 +0100

    hexa

--- srcs/print_hexa.c
+++ srcs/print_hexa.c
@@ -6,26 +6,20 @@
 /*   By: hduflos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 11:10:58 by hduflos           #+#    #+#             */
-/*   Updated: 2024/11/15 12:31:37 by hduflos          ###   ########.fr       */
+/*   Updated: 2024/11/15 14:41:12 by hduflos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "printf.h"
-#include <unistd.h>
+#include "printf.h"
 
-void	print_lowercase_hexa(int n, char *base)
+void	print_lowercase_hexa(unsigned long n, char *base)
 {
-	if (n < 0)
-	{
-		write(1, '-', 1);
-		n = -n;
-	}
 	if (n >= 16)
 		print_lowercase_hexa(n / 16);
 	ft_putchar_fd(base[n % 16], 1);
 }
 
-int	count_hexa(int 	n)
+int	count_hexa(int n)
 {
 	int	count;
 
@@ -51,22 +45,46 @@ int	print_hexa(int n, char c)
 
 	lower_base = "0123456789abcdef";
 	upper_base = "0123456789ABCDEF";
-	if (n =  -2147483648) 
+	if (n == -2147483648)
 	{
 		write(1, "-80000000", 9);
 		return (9);
 	}
 	count = count_hexa(n);
+	if (n < 0)
+	{
+		n = -n;
+		ft_putchar('-', 1);
+	}
 	if (c == 'x')
-		print_hexa(n, lower_base);
+		print_hexa((unsigned long)n, lower_base);
 	if (c == 'X')
-		print_hexa(n, upper_base);
+		print_hexa((unsigned long)n, upper_base);
 	return (count);
 }
 
+int	void_to_hexa(void *p)
+{
+	unsigned long	i;
+	int				count;
+
+	i = (unsigned long)p;
+	count = 1;
+	while (i >= 16)
+	{
+		i /= 16;
+		count++;
+	}
+	i = (unsigned long)p;
+	print_hexa(i, x);
+	return (count);
+}
+
+/*
 int	main()
 {
 	print_lowercase_hexa(68);
 
 	return (0);
 }
+*/
